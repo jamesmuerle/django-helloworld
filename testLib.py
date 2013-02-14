cfbf93f36044dbd07e9d0dcd6bc723412e2c563f
James <jmuerle@jmuerle-ubuntu.(none)>
2013-02-13 18:02:48 -0800

    More timeout

--- testLib.py
+++ testLib.py
@@ -48,9 +48,7 @@ class RestTestCase(unittest.TestCase):
                 sys.exit(1)
             raise
 
-        import socket
-        socket.setdefaulttimeout(100.0)
-        self.conn.sock.settimeout(100.0) # Give time to the remote server to start and respond
+        self.conn.sock.settimeout(1000.0) # Give time to the remote server to start and respond
         resp = self.conn.getresponse()
         data_string = "<unknown"
         try:
--- warmup/errors.py
+++
@@ -1,6 +0,0 @@
-class Errors:
-	SUCCESS = 1 # a success
-	ERR_BAD_CREDENTIALS = -1 # (for login) cannot find the user/password pair in the database
-	ERR_USER_EXISTS = -2 # (for add) trying to add a user that already exists
-	ERR_BAD_USERNAME = -3 # (for add) invalid user name (user name should be non-empty and at most 128 ascii characters long)
-	ERR_BAD_PASSWORD = -4 # (for add) invalid password (password should be at most 128 ascii characters)
